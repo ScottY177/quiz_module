e873f1987283dcabbb46676d5b6c00c4ee1079ae
scottyang <scottyang177@gmail.com>
2022-03-09 10:42:36 -0800

    naming

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='quiz_module',
+    name='quiz',
     version='0.0.1',
     author='Scott',
     author_email='yuy004@ucsd.edu',
@@ -16,7 +16,7 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/ScottY177/quiz_module/issues"
     },
     license='MIT',
-    packages=['quiz_module'],
+    packages=['quiz'],
     package_data = {"": ["*.js", "*.css"]},
     include_package_data = True,
     install_requires=[''],
