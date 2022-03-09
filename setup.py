c604c19f6f81203cc90ef111be4c7063c896611d
scottyang <scottyang177@gmail.com>
2022-03-09 10:05:10 -0800

    update module name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='jupyter-quiz',
+    name='jupyter_quiz',
     version='0.0.1',
     author='Scott',
     author_email='yuy004@ucsd.edu',
