e4e73956e2aa48b94fa1e5e8b88b914a7f3cef85
scottyang <scottyang177@gmail.com>
2022-03-09 10:07:53 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/ScottY177/quiz_module/issues"
     },
     license='MIT',
-    packages=['jupyter_quiz'],
-    install_requires=[''],
+    packages=['jupyter_quiza'],
+    install_requires=['']
 )
