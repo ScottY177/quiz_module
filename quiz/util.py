9cf66d26575bafcb38aefc565076aa9a44b87ad2
scottyang <67733409+ScottY177@users.noreply.github.com>
2022-03-30 10:40:44 -0700

    update util

--- quiz/util.py
+++ quiz/util.py
@@ -1,4 +1,8 @@
 def record_quiz(quiz_id, option_id):
+    """
+    Helper method for recording the quiz to the local machine
+    as a txt file so that we could grade it later on.
+    """
     with open("record.txt", "a+") as f:
         output = {}
         output["quiz_id"] = quiz_id
@@ -6,6 +10,15 @@ def record_quiz(quiz_id, option_id):
         f.write(str(output) + "\n")
 
 def check_quiz_answer(quiz_id, correct_answer):
+    """
+    This method checks the correctness of someone's quiz response
+    by looking at their records.txt
+
+    param:
+    quiz_id: str, the quiz_id that corresponding to the question we are checking
+    correct_answer: int, the index of the correct answers.
+    """
+    correct = False
     with open("record.txt", "r") as f:
         record = f.read().split("\n")
         for i in range(len(record)-2, -1, -1):
@@ -14,4 +27,6 @@ def check_quiz_answer(quiz_id, correct_answer):
             if dictRecord["quiz_id"] == quiz_id:
                 correct_option_id = f"{quiz_id}-{correct_answer}"
                 assert correct_option_id == dictRecord["option_id"], f"The correct option should be: {correct_option_id}"
+                correct = True
                 break
+        assert correct, f"No attempt have made for the question: {quiz_id}"
